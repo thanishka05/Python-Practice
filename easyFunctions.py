b92b69a07bb4efa4a7295a67b34aac5ec2d435cf
thanishka05 <thanishkareddy05@gmail.com>
2025-03-28 16:42:28 +0530

    This file very easy problems on how to work with Python

---
+++ easyFunctions.py
@@ -0,0 +1,33 @@
+# Greet a User
+# a=input("Enter Your Name :")
+# def Greeting():
+#     print(f"Hello {a}")
+# Greeting()
+#  Sum of Two Numbers
+# b=int(input("Enter the 1st number :"))
+# c=int(input("Enter the 2nd number :"))
+# d=b+c
+# def sum():
+#     print(f"The sum of {b} and {c} is {d}")
+# sum()
+# Find Maximum
+# b=int(input("Enter the 1st number :"))
+# c=int(input("Enter the 2nd number :"))
+# def maximum():
+#     d=max(b,c)
+#     print(f"The maximum Number among the numbers you entered is {d}")
+# maximum()
+# Check Even or Odd
+# a= int(input("Enter the Number :"))
+# def Checking():
+#     if a%2 == 0:
+#         print(f"The number {a} is a even number")
+#     else:
+#         print(f"The number {a} is odd number")
+# Checking()
+#  Length of a String
+a=input("Enter a Sentence :")
+def lengths():
+    b=len(a.replace(" ",""))
+    print(f"The Length of the Sentence is {b} letters")
+lengths()
